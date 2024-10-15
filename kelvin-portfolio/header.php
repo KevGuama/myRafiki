0d6a8d8d3c5465e8ca33ce1d079a7bdb572c731e
KevGuama <kevguama@gmail.com>
2024-10-16 02:20:40 +0300

    creating cms firendly structure headerDev_1.0

---
+++ kelvin-portfolio/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <h1><?php bloginfo('name'); ?></h1>
+        <p><?php bloginfo('description'); ?></p>
+    </header>
