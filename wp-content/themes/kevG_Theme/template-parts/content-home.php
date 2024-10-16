20af0ba08bd918d04885384fe8c61fbc5b7716d0
KevGuama <kevguama@gmail.com>
2024-10-17 02:32:20 +0300

    editing kevG_theme permissions to allow wp deployement on theme editing, adding Home pages

--- wp-content/themes/kevG_Theme/template-parts/content-home.php
+++ wp-content/themes/kevG_Theme/template-parts/content-home.php
@@ -1,9 +1,11 @@
-<section class="projects">
-    <h2>My Projects</h2>
-    <!-- Link to the full post -->
-    <ul>
-        <li><a href="#">Project 1</a> - Description of project 1</li>
-        <li><a href="#">Project 2</a> - Description of project 2</li>
-        <li><a href="#">Project 3</a> - Description of project 3</li>
-    </ul>
-</section>
+<?php
+/**
+ * Template part for displaying the home page content.
+ */
+?>
+
+<div class="home-content">
+    <h1>Welcome to Kelvin's Portfolio</h1>
+    <p>This is the homepage of the portfolio site. It provides an overview of Kelvin's work and expertise.</p>
+    <img src="<?php echo get_template_directory_uri(); ?>/images/home-hero.jpg" alt="Home Image">
+</div>
