436e7add02cb83cb72cac2a35abd6a795498d746
KevGuama <kevguama@gmail.com>
2024-10-17 02:02:46 +0300

    move my php custome theme kevG_Theme to wp theme folder for deployment in the cms

--- kevG_theme/template-parts/content-about.php
+++
@@ -1,6 +0,0 @@
-<section class="about-page">
-    <!-- Link to the full post in about page -->
-    <h1>About Kelvin</h1>
-    <p>Kelvin is an engineer who specializes in software, IT support, and digital marketing.</p>
-    <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="Kelvin's Profile Picture">
-</section>
