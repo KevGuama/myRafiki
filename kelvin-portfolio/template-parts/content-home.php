69ff49401d46cb27988e09e2678ee8d9bc32e18b
KevGuama <kevguama@gmail.com>
2024-10-16 03:13:05 +0300

    creating wp cms friendly adding comments homecontent_7.0

---
+++ kelvin-portfolio/template-parts/content-home.php
@@ -0,0 +1,9 @@
+<section class="projects">
+    <h2>My Projects</h2>
+    <!-- Link to the full post -->
+    <ul>
+        <li><a href="#">Project 1</a> - Description of project 1</li>
+        <li><a href="#">Project 2</a> - Description of project 2</li>
+        <li><a href="#">Project 3</a> - Description of project 3</li>
+    </ul>
+</section>
