e3c41a5d9c5bb2beea22528601c57ae0ae73a1d1
KevGuama <kevguama@gmail.com>
2024-10-20 22:28:17 +0300

    Learning from astra wp theme structure and how it's implemented Structure 5.0

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-5.js
@@ -0,0 +1,91 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-b606dc4c' );
+});
+var ssLinksParent = document.querySelector( '.uagb-block-f069584e' );
+ssLinksParent?.addEventListener( 'keyup', function ( e ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link && e.keyCode === 13 ) {
+	handleSocialLinkClick( link );
+}
+});
+
+ssLinksParent?.addEventListener( 'click', function ( e ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link ) {
+	handleSocialLinkClick( link );
+}
+});
+
+function handleSocialLinkClick( link ) {
+var social_url = link.dataset.href;
+var target = "";
+if ( social_url == "mailto:?body=" ) {
+	target = "_self";
+}
+var request_url = "";
+if ( social_url.indexOf("/pin/create/link/?url=") !== -1 ) {
+	request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+} else {
+	request_url = social_url + encodeURIComponent( window.location.href );
+}
+window.open( request_url, target );
+}
+var ssLinksParent = document.querySelector( '.uagb-block-c6adae04' );
+ssLinksParent?.addEventListener( 'keyup', function ( e ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link && e.keyCode === 13 ) {
+	handleSocialLinkClick( link );
+}
+});
+
+ssLinksParent?.addEventListener( 'click', function ( e ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link ) {
+	handleSocialLinkClick( link );
+}
+});
+
+function handleSocialLinkClick( link ) {
+var social_url = link.dataset.href;
+var target = "";
+if ( social_url == "mailto:?body=" ) {
+	target = "_self";
+}
+var request_url = "";
+if ( social_url.indexOf("/pin/create/link/?url=") !== -1 ) {
+	request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+} else {
+	request_url = social_url + encodeURIComponent( window.location.href );
+}
+window.open( request_url, target );
+}
+var ssLinksParent = document.querySelector( '.uagb-block-e5b386bd' );
+ssLinksParent?.addEventListener( 'keyup', function ( e ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link && e.keyCode === 13 ) {
+	handleSocialLinkClick( link );
+}
+});
+
+ssLinksParent?.addEventListener( 'click', function ( e ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link ) {
+	handleSocialLinkClick( link );
+}
+});
+
+function handleSocialLinkClick( link ) {
+var social_url = link.dataset.href;
+var target = "";
+if ( social_url == "mailto:?body=" ) {
+	target = "_self";
+}
+var request_url = "";
+if ( social_url.indexOf("/pin/create/link/?url=") !== -1 ) {
+	request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+} else {
+	request_url = social_url + encodeURIComponent( window.location.href );
+}
+window.open( request_url, target );
+}
+ });
