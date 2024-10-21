6895b0468d69aa3b6d7aa76ef1bec47ef80dca46
KevGuama <kevguama@gmail.com>
2024-10-21 17:12:20 +0300

    footer.php and Js (webpack.config.js)  Debug prototypes for myRafiki theme_4.0

---
+++ wp-content/themes/myRafiki/footer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> myRafiki. All rights reserved.</p>
+    <?php wp_footer(); // Ensures scripts and styles are loaded properly ?>
+</footer>
+</body>
+</html>
