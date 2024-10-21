d5af0592ae4c1e96767de14c9642fbd43736ad68
KevGuama <kevguama@gmail.com>
2024-10-21 16:56:57 +0300

    Creating main index.php prototypes for myRafiki theme_3.0

--- wp-content/themes/myRafiki/index.php
+++ wp-content/themes/myRafiki/index.php
@@ -2,21 +2,31 @@
 /**
  * The main archive template file
  *
- * @package kadence
  */
 
-namespace Kadence;
+get_header(); // Loads the header.php file ?>
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+<div class="content">
+    <?php
+    // Start the loop to check if there are any posts or pages to display
+    if (have_posts()) :
+        
+        // Loop through the available posts/pages
+        while (have_posts()) : the_post();
+            
+            // Load content template from the template-parts folder
+            // The second argument dynamically loads content based on the post format (e.g., gallery, video, etc.)
+            get_template_part('template-parts/content', get_post_format());
+        
+        // End the loop
+        endwhile;
+    
+    // If no posts are found, display a message to the user
+    else :
+        echo '<p>No content found</p>'; // Output a fallback message when there are no posts
+    
+    endif;
+    ?>
+</div>
 
-get_header();
-
-kadence()->print_styles( 'kadence-content' );
-/**
- * Hook for main archive content.
- */
-do_action( 'kadence_archive' );
-
-get_footer();
+<?php get_footer(); // Loads the footer.php file ?>
