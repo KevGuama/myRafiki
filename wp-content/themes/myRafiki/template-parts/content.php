9b09ea9ccfba7dc03f4b0242f5e50b4fa56ab642
KevGuama <kevguama@gmail.com>
2024-10-27 02:43:08 +0300

    Content page logic content-plan-now_debug_7.0

--- wp-content/themes/myRafiki/template-parts/content.php
+++
@@ -1,6 +0,0 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <h2><?php the_title(); // Displays the post title ?></h2>
-    <div class="entry-content">
-        <?php the_content(); // Displays the post content ?>
-    </div>
-</article>
