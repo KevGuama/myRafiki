caf0d88050c058c9084be043eaf546e27949da6f
KevGuama <kevguama@gmail.com>
2024-10-27 02:14:40 +0300

    Trying editing in Gutenberg bulider2.0

--- wp-content/plugins/gutenberg/build/private-apis/index.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'f9dfa9f5ad9f1bbce319');
--- wp-content/plugins/gutenberg/build/router/index.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-polyfill', 'wp-private-apis', 'wp-url'), 'version' => 'f8ae2a3eef0cd387a454');
