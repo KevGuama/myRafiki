1bd42e3857eef40efd09a4fe477b1c033f1c822e
KevGuama <kevguama@gmail.com>
2024-10-27 00:50:02 +0300

    Check if user is logged in_1.0

--- wp-content/themes/myRafiki/template-parts/content-plan-now.php
+++ wp-content/themes/myRafiki/template-parts/content-plan-now.php
@@ -4,3 +4,9 @@
  *
  * This template shows content for the "Plan Now" page.
  */
+
+// Check if user is logged in
+if ( ! is_user_logged_in() ) {
+    wp_redirect( wp_login_url() ); // Redirects to login if not logged in
+    exit;
+}
