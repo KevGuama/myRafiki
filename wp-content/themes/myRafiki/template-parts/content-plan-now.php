131458010aa38ba38b1887c8471e8320701c79f9
KevGuama <kevguama@gmail.com>
2024-10-27 01:19:59 +0300

    adding locations and selecting location from URL or set default_1.0

--- wp-content/themes/myRafiki/template-parts/content-plan-now.php
+++ wp-content/themes/myRafiki/template-parts/content-plan-now.php
@@ -10,3 +10,7 @@ if ( ! is_user_logged_in() ) {
     wp_redirect( wp_login_url() ); // Redirects to login if not logged in
     exit;
 }
+
+// Get selected location from URL or set default
+$location = isset($_GET['location']) ? sanitize_text_field($_GET['location']) : 'Nairobi';
+
