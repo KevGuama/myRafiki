35ee1b458a8c69229959f5ecef5abbc541f834db
KevGuama <kevguama@gmail.com>
2024-10-28 02:17:00 +0300

    Making the Pages Editable in Gutenberg and Accepting User Inputs_3.0

--- wp-content/themes/myRafiki/functions.php
+++ wp-content/themes/myRafiki/functions.php
@@ -67,5 +67,7 @@ add_action( 'wp_enqueue_scripts', 'myrafiki_enqueue_page_specific_styles' );
 // Register block template for Plan Now page.
 function myrafiki_plan_now_block_template()
 
+// Define the block template for the Plan Now page.
+
 ?>
 
