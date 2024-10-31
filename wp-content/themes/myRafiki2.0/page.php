73f5e2529b6aeae7bad079fae540e55222a1aa8b
KevGuama <kevguama@gmail.com>
2024-10-31 15:48:48 +0300

    Create a new my Rafiki theme structure for pages_1.0

--- wp-content/themes/myRafiki2.0/page.php
+++ wp-content/themes/myRafiki2.0/page.php
@@ -4,13 +4,6 @@
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Astra
- * @since 1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
