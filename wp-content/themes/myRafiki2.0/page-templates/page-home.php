d3e609d625d04aede3f9daea6834aeec43b2bf10
KevGuama <kevguama@gmail.com>
2024-10-31 15:56:15 +0300

    Create the Home Page Template File_3.0

--- wp-content/themes/myRafiki2.0/page-templates/page-home.php
+++ wp-content/themes/myRafiki2.0/page-templates/page-home.php
@@ -5,3 +5,19 @@
  *
  * This is the template for the home page.
  */
+
+get_header(); ?>
+
+<div class="site-home">
+    <!-- Gutenberg content will be editable here -->
+    <?php
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post();
+            the_content();
+        }
+    }
+    ?>
+</div>
+
+<?php get_footer(); ?>
