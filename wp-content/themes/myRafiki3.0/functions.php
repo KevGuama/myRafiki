bbf8098495c3b16dcd1446f221ed55e8ccf32ca5
KevGuama <kevguama@gmail.com>
2025-01-08 00:37:07 +0300

    Enabling support for post thumbnails and Gutenberg block editor.

--- wp-content/themes/myRafiki3.0/functions.php
+++ wp-content/themes/myRafiki3.0/functions.php
@@ -8,3 +8,10 @@ if (!defined('ABSPATH')) {
 function myrafiki3_theme_setup() {
     // Enable support for dynamic title tags.
     add_theme_support('title-tag');
+// Enable support for post thumbnails.
+    add_theme_support('post-thumbnails');
+
+    // Enable support for Gutenberg block editor styles.
+    add_theme_support('align-wide');
+    add_theme_support('editor-styles');
+    add_theme_support('responsive-embeds');
