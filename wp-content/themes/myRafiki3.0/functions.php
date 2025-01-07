dcccd981b41b8f66712f4c9d6945d54f4dc8baaa
KevGuama <kevguama@gmail.com>
2025-01-08 00:35:44 +0300

    Adding theme setup functionality.

--- wp-content/themes/myRafiki3.0/functions.php
+++ wp-content/themes/myRafiki3.0/functions.php
@@ -3,3 +3,8 @@
 if (!defined('ABSPATH')) {
     exit;
 }
+
+// Add theme setup functionality.
+function myrafiki3_theme_setup() {
+    // Enable support for dynamic title tags.
+    add_theme_support('title-tag');
