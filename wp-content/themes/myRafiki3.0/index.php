3893b6ca3c86512855268a271fbaf24444d1c933
KevGuama <kevguama@gmail.com>
2025-01-08 01:53:23 +0300

    Looped Through Posts _2.0

--- wp-content/themes/myRafiki3.0/index.php
+++ wp-content/themes/myRafiki3.0/index.php
@@ -42,3 +42,13 @@ get_footer(); // Load the footer template.
                     <div class="entry-content">
                         <?php the_excerpt(); // Displays a summary of the post content. ?>
                     </div>
+<?php while (have_posts()) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); // Adds dynamic post classes ?>>
+                    <h2><a href="<?php the_permalink(); // Links to the full post ?>"><?php the_title(); // Displays the post title ?></a></h2>
+                    <div class="entry-meta">
+                        <span>Posted on <?php echo get_the_date(); // Displays the post date ?></span>
+                    </div>
+                    <div class="entry-content">
+                        <?php the_excerpt(); // Displays a summary of the post content. ?>
+                    </div>
+                </article>
