bb7c177237bf4cd714fcbb1e0aee3a579c01be03
KevGuama <kevguama@gmail.com>
2025-01-08 02:05:40 +0300

    Register widget lace holder

--- wp-content/themes/myRafiki3.0/inc/theme-functions.php
+++ wp-content/themes/myRafiki3.0/inc/theme-functions.php
@@ -3,3 +3,16 @@
 if (!defined('ABSPATH')) {
     exit;
 }
+
+// Register widget areas.
+function myrafiki3_widgets_init() {
+    register_sidebar(array(
+        'name'          => __('Sidebar', 'myrafiki'),
+        'id'            => 'sidebar-1',
+        'description'   => __('Add widgets here.', 'myrafiki'),
+        'before_widget' => '<section class="widget">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ));
+}
