d6a559f1b0a926fbe0a059d3d4eab436022bc6dc
KevGuama <kevguama@gmail.com>
2025-01-08 00:41:53 +0300

    Debug 1.0

--- wp-content/themes/myRafiki3.0/functions.php
+++ wp-content/themes/myRafiki3.0/functions.php
@@ -8,24 +8,25 @@ if (!defined('ABSPATH')) {
 function myrafiki3_theme_setup() {
     // Enable support for dynamic title tags.
     add_theme_support('title-tag');
-// Enable support for post thumbnails.
+
+    // Enable support for post thumbnails.
     add_theme_support('post-thumbnails');
 
     // Enable support for Gutenberg block editor styles.
     add_theme_support('align-wide');
     add_theme_support('editor-styles');
     add_theme_support('responsive-embeds');
- // Register primary navigation menu.
+
+    // Register primary navigation menu.
     register_nav_menus(array(
         'primary' => __('Primary Menu', 'myrafiki'),
     ));
 }
-
 add_action('after_setup_theme', 'myrafiki3_theme_setup');
 
 // Enqueue theme styles and scripts.
 function myrafiki3_enqueue_assets() {
-// Enqueue main stylesheet.
+    // Enqueue main stylesheet.
     wp_enqueue_style('myrafiki-style', get_stylesheet_uri(), array(), '1.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'myrafiki3_enqueue_assets');
