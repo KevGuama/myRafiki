ee846eb0370f0fa7f32fccec72aee391f00218ca
KevGuama <kevguama@gmail.com>
2025-01-08 00:48:46 +0300

    Created  the footer template loading

--- wp-content/themes/myRafiki3.0/index.php
+++ wp-content/themes/myRafiki3.0/index.php
@@ -10,9 +10,12 @@ get_header(); // Load the header template.
 
 if (have_posts()) :
     while (have_posts()) :
+
         the_post(); 
         the_content(); // Display page content.
     endwhile;
 else :
     echo '<p>' . __('No content available.', 'myrafiki') . '</p>';
 endif;
+get_footer(); // Load the footer template.
+?>
