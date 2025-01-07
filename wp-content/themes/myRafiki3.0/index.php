9fade5b262ec84b743a70476056d9aea794d653d
KevGuama <kevguama@gmail.com>
2025-01-08 00:52:47 +0300

    Debug index.php_1,0

--- wp-content/themes/myRafiki3.0/index.php
+++ wp-content/themes/myRafiki3.0/index.php
@@ -6,16 +6,15 @@ if (!defined('ABSPATH')) {
 
 // Basic structure for the index page.
 get_header(); // Load the header template.
-get_header(); // Load the header template.
 
 if (have_posts()) :
     while (have_posts()) :
-
         the_post(); 
         the_content(); // Display page content.
     endwhile;
 else :
     echo '<p>' . __('No content available.', 'myrafiki') . '</p>';
 endif;
+
 get_footer(); // Load the footer template.
 ?>
